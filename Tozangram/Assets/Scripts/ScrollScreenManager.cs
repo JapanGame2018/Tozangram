6b1bc40ff32a7531ca1db33f19928f00a47aa902
Kazuya-Pum <kazuya.pum@gmail.com>
2018-05-16 12:57:59 +0900

    画面の切り替えのみ実装

---
+++ Tozangram/Assets/Scripts/ScrollScreenManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScreenManager : MonoBehaviour
+{
+
+    private Transform m_camera;
+    private RectTransform target;
+
+    private void Start()
+    {
+        m_camera = GameObject.Find("Main Camera").GetComponent<Transform>();
+        target = GetComponent<RectTransform>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(MoveCamera());
+            m_camera.position = new Vector3(target.position.x, target.position.y, -10);
+        }
+    }
+
+    private IEnumerator MoveCamera()
+    {
+
+        yield break;
+    }
+}
