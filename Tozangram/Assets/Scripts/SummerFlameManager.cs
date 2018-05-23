f69b190a7a0511db1c3256dfd2f9b0544e729d26
Kazuya-Pum <kazuya.pum@gmail.com>
2018-05-23 18:55:56 +0900

    タイル切り替え

---
+++ Tozangram/Assets/Scripts/SummerFlameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SummerFlameManager : MonoBehaviour {
+
+    [SerializeField] private GameObject crimbableWall;
+    private TilemapRenderer crimbableRender;
+    private PlatformEffector2D crimbablePlatform;
+
+    private void Start()
+    {
+        crimbableRender = crimbableWall.GetComponent<TilemapRenderer>();
+        crimbablePlatform = crimbableWall.GetComponent<PlatformEffector2D>();
+    }
+
+    public void Desabled()
+    {
+        crimbableRender.enabled = !enabled;
+        crimbablePlatform.useSideFriction = false;
+    }
+
+    public void Enabled()
+    {
+        crimbableRender.enabled = enabled;
+        crimbablePlatform.useSideFriction = true;
+    }
+}
